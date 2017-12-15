e8d86833cba75f017e7f4ada536f0fd91e91afab
yunyin <admin@a25j01437.em21>
2017-12-15 14:16:31 +0800

    New Verison

--- src/model.py
+++ src/model.py
@@ -57,56 +57,47 @@ class Model():
     self.contextW = tf.get_variable(
         "contextW", [2 * config['hidden_size'], config['hidden_size']], dtype = config['data_type'])
 
-
-  def __init__(self, is_training, config, optimizer = None, lr = None):
-    self.config = config
-    batch_size = config['batch_size']
+  def _build_encode(self, input, mask, config):
     hidden_size = config['hidden_size']
-
-    self._optimizer = optimizer
-    self._lr = lr
-
-    with tf.name_scope('inputs'):
-      self.input_data = tf.placeholder(tf.int32, [batch_size, config['src_length']])
-      self.input_mask = tf.placeholder(config['data_type'], [batch_size, config['src_length']])
-      self.target_data = tf.placeholder(tf.int32, [batch_size, config['tgt_length']])
-      self.target_mask = tf.placeholder(config['data_type'], [batch_size, config['tgt_length']])
-
     with tf.name_scope('model/encode'):
       with tf.device("/cpu:0"):
         src_embedding = tf.get_variable(
             "src_embedding", [config['src_vocab_size'], hidden_size], dtype = config['data_type'])
-        inputs = tf.nn.embedding_lookup(src_embedding, self.input_data)
+        encode_inputs = tf.nn.embedding_lookup(src_embedding, input)
       encode_forward = self.lstm_cell(hidden_size)
       encode_backward = self.lstm_cell(hidden_size)
-      batch_mask = tf.reduce_sum(self.input_mask, axis = 1)
+      batch_mask = tf.reduce_sum(mask, axis = 1)
       outputs, output_states = tf.nn.bidirectional_dynamic_rnn(cell_fw = encode_forward,
                                                                cell_bw = encode_backward,
-                                                               inputs = inputs,
+                                                               inputs = encode_inputs,
                                                                sequence_length = tf.cast(batch_mask, tf.int32),
                                                                dtype = config['data_type'])
-      self._encoder = tf.concat(outputs, axis = 2)
+    return tf.concat(outputs, axis = 2)
 
+  def _build_decode(self, input, imask, encode, encode_mask, seq_length, config):
+    hidden_size = config['hidden_size']
     with tf.name_scope('model/decode'):
       with tf.device("/cpu:0"):
         tgt_embedding = tf.get_variable(
             "tgt_embedding", [config['tgt_vocab_size'], hidden_size], dtype = config['data_type'])
-        targets = tf.nn.embedding_lookup(tgt_embedding, self.target_data)
+        decode_inputs = tf.nn.embedding_lookup(tgt_embedding, input)
 
-      self._decode_initial_state = tf.zeros([batch_size, hidden_size], dtype = config['data_type'])
+      batch_size = tf.shape(decode_inputs)[0]
+      decode_state = tf.zeros([batch_size, hidden_size], dtype = config['data_type'])
 
-      decode_state = self._decode_initial_state
-      outputs = []
+      decode_states = []
       with tf.variable_scope("RNN"):
         self.init_decode_parameters(config = config)
-        flattern_targets = tf.reshape(targets, [-1, hidden_size])
-        state_belowx = tf.matmul(flattern_targets, self.state_below_Wx) + self.state_below_bx
-        state_belowx = tf.reshape(state_belowx, [batch_size, config['tgt_length'], 2 * hidden_size])
-        state_below_ = tf.matmul(flattern_targets, self.state_below_W) + self.state_below_b
-        state_below_ = tf.reshape(state_below_, [batch_size, config['tgt_length'], hidden_size])
+        flattern_inputs = tf.reshape(decode_inputs, [-1, hidden_size])
 
-        self.target_mask_ = tf.reshape(self.target_mask, [batch_size, config['tgt_length'], 1])
-        for time_step in range(config['tgt_length'] - 1):
+        state_belowx = tf.matmul(flattern_inputs, self.state_below_Wx) + self.state_below_bx
+        state_belowx = tf.reshape(state_belowx, [batch_size, seq_length, 2 * hidden_size])
+
+        state_below_ = tf.matmul(flattern_inputs, self.state_below_W) + self.state_below_b
+        state_below_ = tf.reshape(state_below_, [batch_size, seq_length, hidden_size])
+
+#        imask_ = tf.reshape(imask, [batch_size, seq_length, 1])
+        for time_step in range(seq_length):
           if time_step > 0: tf.get_variable_scope().reuse_variables()
           # calc ~s(t)
           preact1 = tf.matmul(decode_state, self.gru_hs_W) + self.gru_hs_Wb
@@ -119,24 +110,24 @@ class Model():
           preactx1 = tf.tanh(preactx1 + state_below_[:, time_step, :])
 
           hidden_decode_state = u1 * decode_state + (1. - u1) * preactx1
-          hidden_decode_state = tf.multiply(hidden_decode_state, self.target_mask_[:, time_step]) + \
-                                tf.multiply(decode_state, (1. - self.target_mask_[:, time_step]))
+          hidden_decode_state = imask[:, time_step, None] * hidden_decode_state + \
+                                (1. - imask[:, time_step, None]) * decode_state
 
           # calc attention
-          p_state = tf.matmul(decode_state, self.att_w)
-          flatten_encoder = tf.reshape(self._encoder, [-1, 2 * hidden_size])
-          p_ctx = tf.matmul(flatten_encoder, self.att_u)
+          p_state = tf.matmul(hidden_decode_state, self.att_w)
+          p_ctx = tf.matmul(tf.reshape(encode, [-1, 2 * hidden_size]), self.att_u)
           p_ctx = tf.reshape(p_ctx, [batch_size, config['src_length'], hidden_size])
-          p_state = tf.reshape(p_state, [batch_size, 1, hidden_size])
-          p_state = tf.tanh(p_state + p_ctx)
-          p_state = tf.reshape(p_state, [-1, hidden_size])
-          att_state = tf.matmul(p_state, self.att_v)
+
+          p_state = tf.tanh(p_state[:, None, :] + p_ctx)
+
+          att_state = tf.matmul(tf.reshape(p_state, [-1, hidden_size]), self.att_v)
           att_state = tf.reshape(att_state, [batch_size, config['src_length']])
-          self.alpha = tf.nn.softmax(tf.exp(att_state) * self.input_mask)
+
+          self.alpha = tf.nn.softmax(tf.exp(att_state) * encode_mask)
 
           # C(t)
-          self.alpha = tf.reshape(self.alpha, [batch_size, config['src_length'], 1])
-          context = tf.reduce_sum(self._encoder * self.alpha, axis = 1)
+#          self.alpha = tf.reshape(self.alpha, [batch_size, config['src_length'], 1])
+          context = tf.reduce_sum(encode * self.alpha[:, :, None], axis = 1)
           context = tf.reshape(context, [batch_size, 2 * hidden_size])
 
           # calc s(t)
@@ -151,31 +142,55 @@ class Model():
           preactx2_ = tf.matmul(context, self.contextW)
           preactx2 = tf.tanh(preactx2 + preactx2_)
 
-          decode_state = u2 * decode_state + (1. - u2) * preactx2
-          decode_state = self.target_mask_[:, time_step] * decode_state + \
-                         (1. - self.target_mask_[:, time_step]) * hidden_decode_state
+          decode_state = u2 * hidden_decode_state + (1. - u2) * preactx2
+          decode_state = imask[:, time_step, None] * decode_state + \
+                         (1. - imask[:, time_step, None]) * hidden_decode_state
 
-          outputs.append(decode_state)
+          decode_states.append(decode_state)
+    return decode_states
 
+  def __init__(self, is_training, config, seq_length, optimizer = None, lr = None):
+    hidden_size = config['hidden_size']
+    self._optimizer = optimizer
+    self._lr = lr
+
+    with tf.name_scope('encode_input'):
+      self.encode_input = tf.placeholder(tf.int32, [config['batch_size'], config['src_length']])
+      self.encode_mask = tf.placeholder(tf.float32, [config['batch_size'], config['src_length']])
+
+    self._encoder = self._build_encode(input = self.encode_input, mask = self.encode_mask,
+                                        config = config)
+
+    with tf.name_scope('decode_input'):
+      self.decode_input = tf.placeholder(tf.int32, [config['batch_size'], seq_length])
+      self.decode_imask = tf.placeholder(tf.float32, [config['batch_size'], seq_length])
+
+    self.decode_states = self._build_decode(input = self.decode_input, imask = self.decode_imask,
+                                             encode = self._encoder, encode_mask = self.encode_mask,
+                                             seq_length = seq_length, config = config)
+
+    with tf.name_scope('decode_output'):
+      self.decode_output = tf.placeholder(tf.int32, [config['batch_size'], seq_length])
+      self.decode_omask = tf.placeholder(tf.float32, [config['batch_size'], seq_length])
 
     with tf.name_scope('loss'):
-      output = tf.reshape(tf.stack(axis = 1, values = outputs), [-1, hidden_size])
+      outputs = tf.reshape(tf.stack(axis = 1, values = self.decode_states), [-1, hidden_size])
       softmax_w = tf.get_variable(
           "softmax_w", [hidden_size, config['tgt_vocab_size']], dtype = config['data_type'])
       softmax_b = tf.get_variable(
           "softmax_b", [config['tgt_vocab_size']], dtype = config['data_type'])
-      logits = tf.matmul(output, softmax_w) + softmax_b
+      logits = tf.matmul(outputs, softmax_w) + softmax_b
 
-      logits = tf.reshape(logits, [batch_size, config['tgt_length'] - 1, config['tgt_vocab_size']])
+      logits = tf.reshape(logits, [config['batch_size'], seq_length, config['tgt_vocab_size']])
 
       self.probs = tf.nn.softmax(logits)
 
       loss = tf.contrib.seq2seq.sequence_loss(
           logits = logits,
-          targets = self.target_data[:, 1:],
-          weights = self.target_mask[:, 1:],
+          targets = self.decode_output,
+          weights = self.decode_omask,
           average_across_timesteps=True,
-          average_across_batch=True)
+          average_across_batch=False)
 
       self._cost = cost = tf.reduce_sum(loss)
 
@@ -190,10 +205,6 @@ class Model():
 
     self._new_lr = tf.placeholder(tf.float32, shape=[])
     self._lr_update = tf.assign(self._lr, self._new_lr)
-
-  @property
-  def initial_state(self):
-    return self._initial_state
 
   @property
   def train_op(self):
--- src/nmt.py
+++ src/nmt.py
@@ -15,6 +15,7 @@ import optimizer
 flags = tf.flags
 flags.DEFINE_string("config", None, "Directory of Config File.")
 flags.DEFINE_string("type", None, "Run Type: train/gen")
+flags.DEFINE_string("gen_file", None, "File For Gen")
 flags.DEFINE_integer("task_id", 0, "Task Id for Each workers")
 flags.DEFINE_string("job_name", 'worker', "Job Type: ps/worker")
 FLAGS = flags.FLAGS
@@ -36,26 +37,76 @@ def HParam():
 def run_epoch(sess, model, data, is_training = False):
   data.reset()
   costs = 0
-  iters = 0
   times = 0
+  words = 0
   while data.has_next_batch():
     x_batch, y_batch, x_mask, y_mask = data.next_batch()
-    feed_dict = {model.input_data: x_batch,
-                 model.target_data: y_batch,
-                 model.input_mask: x_mask,
-                 model.target_mask: y_mask}
-    fetches = {"costs": model._cost}
+    feed_dict = {model.encode_input: x_batch,
+                 model.encode_mask: x_mask,
+                 model.decode_input: y_batch[:, :-1],
+                 model.decode_imask: y_mask[:, :-1],
+                 model.decode_output: y_batch[:, 1:],
+                 model.decode_omask: y_mask[:, 1:]}
+    fetches = {"costs": model._cost, "probs": model.probs}
     if is_training: fetches["train_op"] = model.train_op
 
     vals = sess.run(fetches, feed_dict)
-    costs += vals["costs"]
 
-    iters += 1
+    real_index = y_batch[:, 1:].reshape(-1)
+    real_index = [real_index[i] + i * np.shape(vals["probs"])[2] for i in range(len(real_index))]
+    real_probs = vals["probs"].reshape(-1)[real_index]
+    costs += -1.0 * np.sum(np.log(real_probs) * y_mask[:, 1:].reshape(-1))
+
+#    costs += vals["costs"]
+    words += np.sum(y_mask[:, 1:])
     times += 1
     if times % 2000 == 100:
-      tf.logging.info('step %d: training_loss: %.4f' % (times, np.exp(costs / iters)))
+      tf.logging.info('step %d: training_loss: %.4f' % (times, np.power(2, costs / words)))
 
-  return costs / iters
+  return costs / words
+
+def sample_run(model, src_in, max_beam, max_len):
+  pass
+
+def gen(config, gen_file):
+  # load Vocab
+  src_vocab = data_reader.Vocab(vocab_limits = config['src_vocab_size'])
+  src_vocab.load_metadata(config['metadata']['src'])
+  config['src_vocab_size'] = src_vocab.vocab_size()
+
+  tgt_vocab = data_reader.Vocab(vocab_limits = config['tgt_vocab_size'])
+  tgt_vocab.load_metadata(config['metadata']['tgt'])
+  config['tgt_vocab_size'] = tgt_vocab.vocab_size()
+  tf.logging.info(config)
+
+  # create model
+  with tf.name_scope('Genearte'):
+    with tf.variable_scope("Model", reuse = None):
+      gen_model = model.Model(is_training = False,
+                              config = config,
+                              seq_length = 1)
+
+  sv = tf.train.Supervisor(logdir = config['logdir'])
+  sess_config = tf.ConfigProto(allow_soft_placement = True,
+                               log_device_placement = False)
+  sess_config.gpu_options.allow_growth = True
+  sess_config.gpu_options.per_process_gpu_memory_fraction = 0.5
+
+  tf.logging.info('Start Sess')
+
+  for line in open(gen_file):
+    words = line.strip().split(' ')
+    words.insert(0, "<s>")
+    words.append("</s>")
+    masks = [1] * len(words)
+    if len(words) < config['src_length']:
+      masks.extend([0] * (config['src_length'] - len(words)))
+      words.extend(["</s>"] * (config['src_length'] - len(words)))
+    src_vec = [src_vocab.char2id(c) for c in words]
+    out_vec = sample_run(gen_model, src_vec, config['batch_size'])
+    out_words = [tgt_vocab.id2char(c) for c in out_vec]
+    tf.logging.info('Input: %s' % line.strip())
+    tf.logging.info('Output: %s' % (" ".join(out_words)))
 
 def train(config):
   # load Vocab
@@ -85,6 +136,7 @@ def train(config):
     with tf.variable_scope("Model", reuse = None, initializer = initializer):
       train_model = model.Model(is_training = True,
                                 config = config,
+                                seq_length = config['tgt_length'] - 1,
                                 optimizer = opt,
                                 lr = lr)
 
@@ -102,7 +154,7 @@ def train(config):
 
       tf.logging.info('Iter %d Start, Learning_rate: %.4f' % (i, sess.run(train_model.lr)))
       loss = run_epoch(sess, train_model, train_data, is_training = True)
-      tf.logging.info('Iter %d: training_loss: %.4f' % (i, np.exp(loss)))
+      tf.logging.info('Iter %d: training_loss: %.4f' % (i, np.power(2, loss)))
 
 def main(_):
   config = HParam()
@@ -110,6 +162,8 @@ def main(_):
     raise ValueError('run type is train')
   if FLAGS.type == 'train':
     train(config)
+  elif FLAGS.type == 'gen':
+    gen(config, FLAGS.gen_file)
   else:
     raise ValueError('only train is valid')
 
